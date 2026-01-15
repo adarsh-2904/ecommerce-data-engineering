b73ef4294b5c533d5e7ff06bd802870e7f4faf4d
adarsh-2904 <adarsh29041997@gmail.com>
2026-01-15 14:01:25 +0530

    added enableHiveSupport in spark session configuration

--- src/utils/spark_session.py
+++ src/utils/spark_session.py
@@ -20,6 +20,8 @@ def get_spark_session(app_name:str)-> SparkSession:
             "spark.jars",
             "C:/Users/Exavalu/OneDrive - exavalu/jar/postgresql-42.7.3.jar"
         )
+        .config("spark.sql.warehouse.dir", "C:/Users/Exavalu/OneDrive - exavalu/airflow_practice/ecommerce-data-engineering/src/data_warehouse")
+        .enableHiveSupport()    #enabled Hive suport to store metadata in a metastore. It uses the derby database by default.
         .getOrCreate()
     )
     return spark
